ee94f7f30589793474198a894b1ba03889f51533
Stefan K <40999112+s-tefan@users.noreply.github.com>
2023-12-11 08:57:05 +0100

    formaterad

--- 11/aoc_2023_11.py
+++ 11/aoc_2023_11.py
@@ -2,9 +2,11 @@
 # S-tefan
 import time
 
+
 def read_input(filename):
     with open(filename) as f:
         return [line.rstrip() for line in f.readlines()]
+
 
 def empty_rows(lines):
     n = []
@@ -12,6 +14,7 @@ def empty_rows(lines):
         if all(c == '.' for c in line):
             n.append(k)
     return n
+
 
 def empty_cols(lines):
     l = len(lines[0])
@@ -21,19 +24,18 @@ def empty_cols(lines):
             n.append(k)
     return n
 
-def galaxies(lines, factor = 1):
+
+def galaxies(lines, factor=1):
     e_rows, e_cols = empty_rows(lines), empty_cols(lines)
     g = []
     gg = []
     for r, line in enumerate(lines):
-        g += [(r,c) for (c, ch) in enumerate(line) if ch == '#']
-    for (r,c) in g:
+        g += [(r, c) for (c, ch) in enumerate(line) if ch == '#']
+    for (r, c) in g:
         rr = r + (factor-1)*[k < r for k in e_rows].count(True)
         cc = c + (factor-1)*[k < c for k in e_cols].count(True)
-        gg.append((rr,cc))
+        gg.append((rr, cc))
     return gg
-
-
 
 
 def partone(lines):
@@ -44,6 +46,7 @@ def partone(lines):
             dists.append(abs(g1[0]-g2[0]) + abs(g1[1]-g2[1]))
     return sum(dists)
 
+
 def parttwo(lines):
     gals = galaxies(lines, 1000000)
     dists = []
@@ -51,6 +54,7 @@ def parttwo(lines):
         for g2 in gals[:k]:
             dists.append(abs(g1[0]-g2[0]) + abs(g1[1]-g2[1]))
     return sum(dists)
+
 
 inp = read_input("input.txt")
 a, b = time.perf_counter(), time.process_time()
