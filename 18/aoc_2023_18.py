7f389671626ebb5ce9682a924024644aa9c3f707
Stefan K <40999112+s-tefan@users.noreply.github.com>
2023-12-18 19:45:39 +0100

    18 python funkis

--- 18/aoc_2023_18.py
+++ 18/aoc_2023_18.py
@@ -10,16 +10,28 @@ def read_input(filename):
 
 
 posts = read_input("input.txt")
-trench = [(0, 0)]
 dirs = {'U': (-1, 0), 'D': (1, 0), 'R': (0, 1), 'L': (0, -1)}
 
-r, c, s, a = 0, 0
+
+# 1
+r, c, s, a = 0, 0, 0, 0
 for post in posts:
     ds = int(post[1])
     dr, dc = (ds*x for x in dirs[post[0]])
     r, c = r + dr, c + dc
     a += c*dr
     s += ds
+a += (s + 2)//2
 print(a)
+
+# 2
+newdirs = [dirs[x] for x in 'RDLU']
+r, c, s, a = 0, 0, 0, 0
+for post in posts:
+    ds = int(post[2][2:7], 16)
+    dr, dc = (ds*x for x in newdirs[int(post[2][7])])
+    r, c = r + dr, c + dc
+    a += c*dr
+    s += ds
 a += (s + 2)//2
 print(a)
